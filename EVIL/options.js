f3b24f06b86cb2bf5d97a3280bdc69da8bf1918d
gsitcia <gsitcia@gmail.com>
2019-08-07 02:33:12 -0500

    finished

--- EVIL/options.js
+++ EVIL/options.js
@@ -1,7 +1,28 @@
+function ov(v) {
+    var z = document.createElement('option');
+    z.value = v;
+    return z;
+}
+
 chrome.storage.local.get(['id'], function(e) {
-    document.getElementById('id').value = e.id;
+    if (e.id) {
+        chrome.management.get(e.id,function(r) {
+            document.getElementById('name').value = r.name;
+            document.getElementById('success').innerText = 'Currently launching: '+r.name;
+        });
+    }
 });
 
-document.getElementById('id').addEventListener('keyup',function() {
-    chrome.storage.local.set({id:document.getElementById('id').value});
+chrome.management.getAll(function(l) {
+    var apps = l.filter(i=>i.type.indexOf('app')!=-1);
+    apps.forEach(i=>document.getElementById('names').appendChild(ov(i.name)));
+    document.getElementById('submit').addEventListener('click',function() {
+        var id = apps.filter(i=>i.name==document.getElementById('name').value);
+        if (id.length > 0) {
+            chrome.storage.local.set({id:id[0].id},function() {
+                var success = document.getElementById('success');
+                success.innerText = chrome.runtime.lastError || ('Currently launching: '+id[0].name);
+            });
+        }
+    });
 });
